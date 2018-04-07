82492fbf7bc633d7216c1a756fbfbd9c06e87f6b
guungofficial <guungofficial@gmail.com>
2018-04-07 07:02:35 +0700

    Nambah komen

--- application/models/M_UI.php
+++ application/models/M_UI.php
@@ -2,6 +2,9 @@
 
 class M_UI extends CI_Model {
     function percentColor($percentage) {
+        /*
+            https://stackoverflow.com/questions/6394304/algorithm-how-do-i-fade-from-red-to-green-via-yellow-using-rgb-values
+        */
         $r = ($percentage > 50 ? 1 - 2 * ($percentage - 50) / 100.0 : 1.0) * 255;
         $g = ($percentage > 50 ? 1.0 : 2 *$percentage / 100.0) * 255;
         return "rgb({$r},{$g},45)";
