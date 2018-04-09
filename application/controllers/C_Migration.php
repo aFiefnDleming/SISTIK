7a6722e46f7e8173581e948be6b47c9353fcc463
guungofficial <guungofficial@gmail.com>
2018-04-09 11:33:22 +0700

    Migrate

--- application/controllers/C_Migration.php
+++ application/controllers/C_Migration.php
@@ -1,12 +1,16 @@
 <?php defined("BASEPATH") or exit("No direct script access allowed");
 
-class Migrate extends CI_Controller{
+class C_Migration extends CI_Controller{
+
+    public function __construct(){
+      parent:: __construct();
+        if(empty($this->session->userdata('ses_username'))) redirect(base_url("l"));
+  	}
 
     public function index($version){
-        $this->load->library("migration");
-
-      if(!$this->migration->version($version)){
-          show_error($this->migration->error_string());
-      }
+            $this->load->library("migration");
+          if(!$this->migration->version($version)){
+              show_error($this->migration->error_string());
+          }
     }
 }
